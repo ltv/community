965140938c2649efaa8d0b27734eab0d1280aa92
henry0hai <henry0hai@gmail.com>
2020-03-11 11:54:36 +0700

    not working, but import full services

--- packages/backend/utils/constants.ts
+++ packages/backend/utils/constants.ts
@@ -3,9 +3,32 @@ export const SERVICE_GATEWAY = 'gateway';
 export const SERVICE_SYSTEM = 'system';
 export const SERVICE_USER = 'user';
 export const SERVICE_AUTH = 'auth';
+
 // Service name (E)
 
 // ROLES (S)
 export const ROLE_EVERYONE = '$everyone';
 export const ROLE_OWNER = '$owner';
 // ROLES (E)
+
+// ERROR (S)
+export const ERR_ENTITY_NOT_FOUND = 'ERR_ENTITY_NOT_FOUND';
+export const ERR_USER_NOT_FOUND = 'ERR_USER_NOT_FOUND';
+export const ERR_ACCOUNT_NOT_VERIFIED = 'ERR_ACCOUNT_NOT_VERIFIED';
+export const ERR_ACCOUNT_DISABLED = 'ERR_ACCOUNT_DISABLED';
+export const ERR_USR_OR_EML_EXISTED = 'ERR_USR_OR_EML_EXISTED';
+export const ERR_INVALID_TOKEN = 'ERR_INVALID_TOKEN';
+export const ERR_INTERNALL_ERROR = 'ERR_INTERNALL_ERROR';
+export const NO_LOGGED_IN_USER = 'NO_LOGGED_IN_USER';
+export const ERR_USR_NOT_EXISTED = 'ERR_USR_NOT_EXISTED';
+export const ERR_REQUIRED_FIELD_MISSING = 'ERR_REQUIRED_FIELD_MISSING';
+export const VALIDATION_ERROR = 'VALIDATION_ERROR';
+
+// SCHEMA NAME (S)
+export const SCHEMA_AUTH = 'auth';
+
+// TABLE NAME (S)
+export const AUTH_USR = 'AUTH_USR';
+export const AUTH_TOKEN = 'AUTH_TOKEN';
+export const ADM_USR = 'ADM_USR';
+export const ADM_SUBS = 'ADM_SUBS';
